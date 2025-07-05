d063a0770ccbbba44da9fe769a59edb0c32805a5
wenhao.wang <wenhao.wang1@bytedance.com>
2025-07-05 16:14:31 -0700

    Initial Commit

---
+++ SpotSeeker/SpotSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotSeekerApp.swift
+//  SpotSeeker
+//
+//  Created by ByteDance on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
